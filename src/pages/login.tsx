a31bf3a321381d89e8b469f1319373955202ba75
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-02-20 09:31:39 +0000

    Add show password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import Layout from "../components/Layout";
 
-import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { Row, Col, InputGroup, Form, Button } from "react-bootstrap";
 import Head from "next/head";
 import styles from "../styles/login.module.css";
 import Link from "next/link";
@@ -16,8 +16,9 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     try {
@@ -50,101 +51,77 @@ const Login = () => {
           <title>Log In</title>
         </Head>
         <div className={styles.wrapper}>
-          <Row className={styles.row}>
-            <Col md={5}>
-              <Side
-                children={
-                  <Container>
-                    <Form
-                      onSubmit={handleSubmit}
-                      className={`${styles.form} ${styles.mobileOnly}`}
-                    >
-                      <p className={styles.title}>Sign In</p>
-                      <Form.Group controlId="email1">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                          type="email"
-                          onChange={(e) => setEmail(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-                      <Form.Group controlId="password1">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          onChange={(e) => setPassword(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-                      <Row>
-                        <Col>
-                          {" "}
-                          <Button
-                            className={styles.btnMain}
-                            type="submit"
-                            variant="primary"
-                            disabled={loading}
-                          >
-                            Sign In
-                          </Button>
-                        </Col>
-                        <Col className={styles.noAccount}>
-                          Don't have an account? <br />{" "}
-                          <span>
-                            <Link href="/register">Create One</Link>
-                          </span>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </Container>
-                }
-              />
-            </Col>
-            <Col md={7}>
-              <Container>
-                <div
-                  className={`${styles.formContainer} ${styles.desktopOnly}`}
-                >
-                  <Form
-                    onSubmit={handleSubmit}
-                    className={`${styles.form} ${styles.desktopOnly}`}
+          <div className={`${styles.formContainer}`}>
+            <Form
+              onSubmit={handleSubmit}
+              className={`${styles.form} ${styles.desktopOnly}`}
+            >
+              <h5 className={styles.title}>Sign In</h5>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <InputGroup>
+                  {" "}
+                  <Form.Control
+                    type={show ? "text" : "password"}
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></Form.Control>
+                  <Button variant="secondary" onClick={(e) => setShow(!show)}>
+                    {show ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-eye-slash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                        <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299l.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                        <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884l-12-12 .708-.708 12 12-.708.708z" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-eye"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                    )}
+                  </Button>
+                </InputGroup>
+              </Form.Group>
+              <Row>
+                <Col md={6}>
+                  {" "}
+                  <Button
+                    className={`mb-2 ${styles.btnMain}`}
+                    type="submit"
+                    variant="primary"
                   >
-                    <h5 className={styles.title}>Sign In</h5>
-                    <Form.Group controlId="email">
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Row>
-                      <Col>
-                        {" "}
-                        <Button
-                          className={styles.btnMain}
-                          type="submit"
-                          variant="primary"
-                        >
-                          Sign In
-                        </Button>
-                      </Col>
-                      <Col className={styles.noAccount}>
-                        Don't have an account? <br />{" "}
-                        <span>
-                          <Link href="/register">Create One</Link>
-                        </span>
-                      </Col>
-                    </Row>
-                  </Form>
-                </div>
-              </Container>
-            </Col>
-          </Row>
+                    Sign In
+                  </Button>
+                </Col>
+                <Col md={6} className={styles.noAccount}>
+                  Don't have an account? <br />{" "}
+                  <span>
+                    <Link href="/register">Create One</Link>
+                  </span>
+                </Col>
+              </Row>
+            </Form>
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
 
-import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { Row, Col, Container, InputGroup, Form, Button } from "react-bootstrap";
 import Head from "next/head";
 import styles from "../styles/register.module.css";
 import { useRouter } from "next/router";
@@ -21,8 +21,9 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const handleSubmit = async (event: any) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     if (confirmPassword === password) {
       try {
@@ -60,198 +61,158 @@ const Register = () => {
           <title>Create Account</title>
         </Head>
         <div className={styles.wrapper}>
-          <Row className={styles.row}>
-            <Col md={5}>
-              <Side
-                children={
-                  <Container>
-                    <Form
-                      onSubmit={handleSubmit}
-                      className={`${styles.form} ${styles.mobileOnly}`}
-                    >
-                      <p className={styles.title}>Create Account</p>
-                      <Row>
-                        <Col md={6}>
-                          <Form.Group controlId="name">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control
-                              type="text"
-                              value={name}
-                              onChange={(e) => setName(e.target.value)}
-                            ></Form.Control>
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group controlId="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                              type="text"
-                              value={username}
-                              onChange={(e) => setUserName(e.target.value)}
-                            ></Form.Control>
-                          </Form.Group>
-                        </Col>
-                      </Row>
-
-                      <Row>
-                        <Col md={6}>
-                          <Form.Group controlId="email1">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control
-                              type="email"
-                              value={email}
-                              onChange={(e) => setEmail(e.target.value)}
-                            ></Form.Control>
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group controlId="gender1">
-                            <Form.Label>Gender</Form.Label>
-                            <Form.Control
-                              type="text"
-                              value={gender}
-                              onChange={(e) => setGender(e.target.value)}
-                            ></Form.Control>
-                          </Form.Group>
-                        </Col>
-                      </Row>
-
-                      <Form.Group controlId="password1">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-
-                      <Form.Group controlId="confirmPassword1">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          value={confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-
-                      <Row>
-                        <Col md={5}>
-                          <Button
-                            className={styles.btnMain}
-                            type="submit"
-                            variant="primary"
-                          >
-                            Create Account
-                          </Button>
-                        </Col>
-                        <Col md={7} className={styles.noAccount}>
-                          Have an account already? <br />{" "}
-                          <span>
-                            <Link href="/login">Log in</Link>
-                          </span>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </Container>
-                }
-              />
-            </Col>
-            <Col md={7}>
-              <Container>
-                <div
-                  className={`${styles.formContainer} ${styles.desktopOnly}`}
-                >
-                  <Form
-                    onSubmit={handleSubmit}
-                    className={`${styles.form} ${styles.desktopOnly}`}
-                  >
-                    <p className={styles.title}>Create Account</p>
-                    <Row>
-                      <Col md={6}>
-                        <Form.Group controlId="name">
-                          <Form.Label>Name</Form.Label>
-                          <Form.Control
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Col>
-                      <Col md={6}>
-                        <Form.Group controlId="username1">
-                          <Form.Label>Username</Form.Label>
-                          <Form.Control
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUserName(e.target.value)}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Col>
-                    </Row>
-
-                    <Row>
-                      <Col md={6}>
-                        <Form.Group controlId="email">
-                          <Form.Label>Email Address</Form.Label>
-                          <Form.Control
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Col>
-                      <Col md={6}>
-                        <Form.Group controlId="gender">
-                          <Form.Label>Gender</Form.Label>
-                          <Form.Control
-                            type="text"
-                            value={gender}
-                            onChange={(e) => setGender(e.target.value)}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Col>
-                    </Row>
-
-                    <Form.Group controlId="password">
-                      <Form.Label>Password</Form.Label>
+          <Container>
+            <div className={`${styles.formContainer} ${styles.desktopOnly}`}>
+              <Form
+                onSubmit={handleSubmit}
+                className={`${styles.form} ${styles.desktopOnly}`}
+              >
+                <p className={styles.title}>Create Account</p>
+                <Row>
+                  <Col md={6}>
+                    <Form.Group controlId="name">
+                      <Form.Label>Name</Form.Label>
                       <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                       ></Form.Control>
                     </Form.Group>
-
-                    <Form.Group controlId="confirmPassword">
-                      <Form.Label>Confirm Password</Form.Label>
+                  </Col>
+                  <Col md={6}>
+                    <Form.Group controlId="username1">
+                      <Form.Label>Username</Form.Label>
                       <Form.Control
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUserName(e.target.value)}
                       ></Form.Control>
                     </Form.Group>
+                  </Col>
+                </Row>
 
-                    <Row>
-                      <Col md={5}>
-                        <Button
-                          className={styles.btnMain}
-                          type="submit"
-                          variant="primary"
+                <Row>
+                  <Col md={6}>
+                    <Form.Group controlId="email">
+                      <Form.Label>Email Address</Form.Label>
+                      <Form.Control
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                  <Col md={6}>
+                    <Form.Group controlId="gender">
+                      <Form.Label>Gender</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <InputGroup>
+                    {" "}
+                    <Form.Control
+                      type={show ? "text" : "password"}
+                      onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                    <Button variant="danger" onClick={(e) => setShow(!show)}>
+                      {show ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-eye-slash"
+                          viewBox="0 0 16 16"
                         >
-                          Create Account
-                        </Button>
-                      </Col>
-                      <Col md={7} className={styles.noAccount}>
-                        Have an account already? <br />{" "}
-                        <span>
-                          <Link href="/login">Log in</Link>
-                        </span>
-                      </Col>
-                    </Row>
-                  </Form>
-                </div>
-              </Container>
-            </Col>
-          </Row>
+                          <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                          <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299l.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                          <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884l-12-12 .708-.708 12 12-.708.708z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-eye"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                        </svg>
+                      )}
+                    </Button>
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="confirmPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <InputGroup>
+                    {" "}
+                    <Form.Control
+                      value={confirmPassword}
+                      type={show ? "text" : "password"}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                    <Button variant="danger" onClick={(e) => setShow(!show)}>
+                      {show ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-eye-slash"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                          <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299l.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                          <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884l-12-12 .708-.708 12 12-.708.708z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-eye"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                        </svg>
+                      )}
+                    </Button>
+                  </InputGroup>
+                </Form.Group>
+
+                <Row>
+                  <Col md={5}>
+                    <Button
+                      className={styles.btnMain}
+                      type="submit"
+                      variant="primary"
+                    >
+                      Register
+                    </Button>
+                  </Col>
+                  <Col md={7} className={styles.noAccount}>
+                    Have an account already? <br />{" "}
+                    <span>
+                      <Link href="/login">Log in</Link>
+                    </span>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+          </Container>
         </div>
       </main>
     </Layout>
