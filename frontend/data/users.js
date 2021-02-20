62ff37519abf8862264b1659e3d1777b0675f486
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-02-20 02:42:21 +0000

    Start seeder script

--- frontend/data/users.js
+++ frontend/data/users.js
@@ -1,5 +1,34 @@
+import bcrypt from "bcryptjs";
+
 const users = [
   {
-    name: "Abdul ",
+    name: "Derek Oware",
+    email: "derekoware47@gmail.com",
+    spassword: bcrypt.hashSync("derekoware2021", 10),
+    isAdmin: true,
+    isModerator: true,
+    department: "Computer Science & Engineering",
+    username: "dchole",
+    phone: "0248245692",
+  },
+  {
+    name: "Oduro Twumasi John Barnes",
+    email: "ohenesetwumasi@gmail.com",
+    password: bcrypt.hashSync("jaybarnes2021", 10),
+    isAdmin: true,
+    isModerator: true,
+    department: "Computer Science & Engineering",
+    username: "barnes",
+    phone: "0543288549",
+  },
+  {
+    name: "Akakpo Prince",
+    email: "deon@gmail.com",
+    password: bcrypt.hashSync("prince2021", 10),
+    isAdmin: true,
+    isModerator: true,
+    department: "Renewable Energy Engineering",
+    username: "deon",
+    phone: "0248245692",
   },
 ];
