f065507c3c8e90047ed8cc383159714098d2d453
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2021-02-21 23:25:54 +0000

    Update homepage

---
+++ src/components/Post.tsx
@@ -0,0 +1,49 @@
+import styles from "../styles/post.module.css";
+import { Image } from "react-bootstrap";
+const Post = ({ post }) => {
+  return (
+    <div className={styles.postContainer}>
+      <Image
+        className={` ${styles.avatar}`}
+        alt={post.author.username}
+        src={post.author.avatar}
+      />
+      <div>
+        <div className={styles.userinfo}>
+          <span className={`${styles.name} `}>
+            <strong>{post.author.name}</strong>
+          </span>
+          <span className={`${styles.username}`}>@{post.author.username}</span>
+          <strong className={styles.date}>
+            {new Date().toLocaleDateString()}
+          </strong>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+            minima aperiam beatae vel minus, recusandae, quod blanditiis eveniet
+            in facilis eaque animi veniam? Dolor sit repellat assumenda
+            voluptatum? Molestiae ad inventore aliquid cupiditate aspernatur
+            distinctio eum id, nam voluptatum commodi?
+          </p>
+          <div className={styles.reactions}>
+            <span className={`${styles.upvotes} mr-3`}>
+              <i className="bi bi-hand-thumbs-up"></i> {post.upvotes}
+            </span>
+            <span className={`${styles.downvotes} mr-3`}>
+              <i className="bi bi-hand-thumbs-down"></i> {post.downvotes}
+            </span>
+            <span className={`${styles.comments} mr-3`}>
+              <i className="bi bi-chat-square-text"></i> {post.comments.length}
+            </span>
+            <span className={`${styles.share} mr-3`}>
+              <i className="bi bi-share"></i> {}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
---
+++ src/data/posts.js
@@ -0,0 +1,168 @@
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: "John Barnes",
+      username: "jaybarnes",
+      avatar: "/uploads/barnes2.png",
+    },
+
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "Maureen Peprah",
+          username: "maureen",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 2,
+    author: {
+      name: "Derek Oware",
+      username: "dchole",
+      avatar: "/uploads/bunny.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "John Barnes",
+          username: "jaybarnes",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        avatar: "/uploads/avatar2.png",
+        pvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 3,
+    author: {
+      name: "John Barnes",
+      username: "jaybarnes",
+      avatar: "/uploads/barnes2.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "Rex Osei",
+          username: "rex",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 4,
+    author: {
+      name: "Rex Osei",
+      username: "rex",
+      avatar: "/uploads/avatar.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "John Barnes",
+          username: "jaybarnes",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 5,
+    author: {
+      name: "John Barnes",
+      username: "jaybarnes",
+      avatar: "/uploads/barnes2.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "Rex Osei",
+          username: "rex",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 3,
+    author: {
+      name: "John Barnes",
+      username: "jaybarnes",
+      avatar: "/uploads/avatar.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "Rex Osei",
+          username: "rex",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+  {
+    id: 3,
+    author: {
+      name: "John Barnes",
+      username: "jaybarnes",
+      avatar: "/uploads/avatar.png",
+    },
+    text: "Anyone with a copy of the basic mechanics handout?",
+    upvotes: 5,
+    downvotes: 2,
+    comments: [
+      {
+        author: {
+          name: "Rex Osei",
+          username: "rex",
+          avatar: "/uploads/avatar.png",
+        },
+        text: "Ask google",
+        upvotes: 3,
+        downvotes: 11,
+      },
+    ],
+  },
+];
+
+export default posts;
